1d17e196f0bacc2ea3826d3c0ad90850b424619e
Beyza Keser <beyzaaakeser@gmail.com>
2024-08-07 19:04:27 +0300

    loading

--- src/component/Loading.jsx
+++ src/component/Loading.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
-
+import "../assets/css/loading.css"
 const Loading = () => {
-  return <p>Loading ... </p>;
+  return (
+    <div class="ring d-flex align-items-center justify-content-center">
+      Loading
+      <span className='loading-span'></span>
+    </div>
+  );
 };
 
 export default Loading;
